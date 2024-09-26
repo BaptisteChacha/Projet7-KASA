ae9fc21de2113cb2308fe6af1919298bea5589c1
Chalon <baptiste.chalon@gmail.com>
2024-09-26 12:42:00 +0200

    fix recuperation données localstorage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,10 @@
 import Header from '../Components/Header.jsx'
-import Card from '../Components/Card.jsx'
+import Card from '../Components/Card.jsx';
 import img from '../Images/IMG.png'
 import '../Style/Header.css';
 import Footer from '../Components/Footer.jsx';
 import '../Style/Home.css';
-import {useEffect, /*useState*/} from 'react';
+import {useEffect, useState, /*useState*/} from 'react';
 import { Link } from 'react-router-dom'
 import styled from 'styled-components'
 
@@ -16,10 +16,13 @@ const StyledLinkFooter = styled(Link)
 `
 
 function Home() {
-    useEffect(() => {
-        fetch('https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/Front-End+V2/P9+React+1/logements.json', {mode: 'no-cors'})
+   // var localS = JSON.parse(localStorage.getItem("items"));
+    //console.log(localS)
+    useState(() => {
+        fetch('https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/Front-End+V2/P9+React+1/logements.json',)
             .then((response) => response.json()
             .then((response) => window.localStorage.setItem("items", JSON.stringify(response)))
+            .then(console.log(localStorage))
             .catch((error) => console.log(error))
     )}, [] )
     return (
