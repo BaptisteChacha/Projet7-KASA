fd208a45bd7abec7f8177f2d0db36c2f9fda3fca
Chalon <baptiste.chalon@gmail.com>
2025-05-10 23:58:10 +0200

    modifs media queries

--- src/index.js
+++ src/index.js
@@ -1,30 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './Style/index.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
-import Home from './pages/Home'
-import Logement from './pages/D_Fiche-Logement'
-import A_Propos from './pages/A_propos'
-import Error from './pages/D_404'
-//import detail from './Components/Détail'
+import Home from './pages/Home';
+import Logement from './pages/D_Fiche-Logement';
+import A_Propos from './pages/A_propos';
+import Error from './pages/D_404';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+
 root.render(
   <React.StrictMode>
-        <Router>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/Logement/:id" element={<Logement />} />
-                <Route path="*" element={<Error />} />
-                <Route path='/A_Propos' element={<A_Propos />} />
-            </Routes>
-        </Router>
-    </React.StrictMode>,
-document.getElementById('root')
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Logement/:id" element={<Logement />} />
+        <Route path="/A_Propos" element={<A_Propos />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </Router>
+  </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
