12d23b9310a871d2fba8dc4c93c12b4fd69e5b1b
Chalon <baptiste.chalon@gmail.com>
2025-05-27 20:04:28 +0200

    modifs page erreur + dimension maquette

--- src/pages/D_404.jsx
+++ src/pages/D_404.jsx
@@ -4,8 +4,9 @@ import '../Style/Erreur.css';
 
 function Error() {
     return (
-        <div>
+        <div className='container_error'>
             <Header></Header>
+            
             <p className='erreur-404'> 404 </p>
             <h1 className='erreur'>Oups la page que vous demandez n'existe pas</h1>
             <a href='http://localhost:3000/'> <p className='retour-accueil'> Retour sur la page d'accueil </p> </a>
